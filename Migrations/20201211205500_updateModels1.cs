8d170f9d14595c9b69ecf308700b28346abf5c39
Jan Piotrowski <j.piotrowski@millionmonkeys.pl>
2020-12-11 22:10:16 +0100

    Wip

---
+++ Migrations/20201211205500_updateModels1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace Z01.Migrations
+{
+    public partial class updateModels1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ActivityId",
+                table: "Activities",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(767)")
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivityId",
+                table: "Activities",
+                type: "varchar(767)",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
